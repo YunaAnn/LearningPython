8775c6555f15e7eaa6ea3842eb69f27da1361417
YunaAnn <ann.grzywnowicz@gmail.com>
2021-08-05 18:19:12 +0200

    1-My-first-script.py

---
+++ 1-My-first-script.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#done in The Jupyter Notebook (browser)
+
+# In[1]:
+
+
+print('Hello world!')
+
+
+# In[7]:
+
+
+print('Celsius to Fahrenheit: 20°C =', 32+9/5*20, '°F')
+
+
+# In[9]:
+
+
+print('Celsius to Kelvin: 20°C =', 20+273.15, 'K')
+
+
+# In[11]:
+
+
+print('Kilometers to Miles: 100km =', 100*0.621371192 , 'mi')
+
+
+# In[ ]:
+
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # LearningPython
+
+1. 1-My-first-script.py - done in The Jupyter Notebook (browser)
