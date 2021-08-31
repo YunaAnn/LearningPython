7e5db48891ec85b9e0a2960e3f218fc607a65992
YunaAnn <ann.grzywnowicz@gmail.com>
2021-08-31 22:10:17 +0200

    Basics/String.py

---
+++ Basics/String.py
@@ -0,0 +1,21 @@
+
+
+def string_simple_examples():
+    string1 = 'Hello world!'
+    string2 = "Hello world!"
+    print(string1)
+    print(string2)
+    # multiline strings use triple quotes
+    string3 = '''
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pulvinar ac nulla ac elementum. 
+    Phasellus at fermentum nibh. Nulla ornare mauris urna, vitae euismod arcu vestibulum ultrices.
+    Mauris eget leo feugiat, dignissim est in, tincidunt velit. Mauris imperdiet metus in luctus dignissim. 
+    Pellentesque fermentum odio et cursus iaculis. Morbi id tortor ac turpis pharetra tempus. 
+    Fusce vitae ultrices quam. Etiam gravida sagittis dolor sit amet finibus.
+    '''
+    print(string3)
+
+
+if __name__ == '__main__':
+    string_simple_examples()
+
